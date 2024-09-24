3334f2f10174f812335622b1bff3b49add3fc851
Eugene Kim <eugene02876@gmail.com>
2024-09-24 15:07:34 +0900

    chore: eslint 세부 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
+  requireConfigFile: false,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+  },
 };
