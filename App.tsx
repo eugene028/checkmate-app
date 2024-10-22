1160642b380f19d4e9f87e30a1bed3ec12cc71ff
Eugene Kim <eugene02876@gmail.com>
2024-10-23 03:00:06 +0900

    feat: 하단 Navigation Tab Bar 생성

--- App.tsx
+++ App.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import {ThemeProvider} from '@emotion/react';
 import {theme} from './src/styles/theme';
 import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from 'components/Home';
+import Modal from 'components/Modal';
 import {createStackNavigator} from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Group screenOptions={{}}>
+            <Tab.Screen name="checkModal" component={Modal} />
+          </Tab.Group>
+        </Tab.Navigator>
       </NavigationContainer>
     </ThemeProvider>
   );
---
+++ src/components/Modal.tsx
@@ -0,0 +1,8 @@
+import {View} from 'react-native';
+import WebView from 'react-native-webview';
+
+const Modal = () => {
+  return <div>모달모달</div>;
+};
+
+export default Modal;
